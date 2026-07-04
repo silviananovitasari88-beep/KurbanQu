b4108670b8487fd5750c5a8df2814f1add1a454c
flooo-alt <bunga.sadiyah30@gmail.com>
2026-07-04 08:39:06 +0700

    benerin config LG

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/bootstrap.js', 'resources/views/admin/dashboard.blade.php', 'resources/views/auth/login.blade.php', 'resources/views/welcome.blade.php'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/bootstrap.js'],
             refresh: true,
         }),
         tailwindcss(),
